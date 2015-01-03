602801c2f0cd1081ca2423c3f6af6e4e1131cdfd
Masato Yamaguchi <karateka2000@gmail.com>
2015-01-03 16:27:39 +0900

    modified benchmark script.

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'benchmark'
 require 'redis'
 require 'redis-asm'
+require 'json'
 
 dic = 
   File.read(File.expand_path('../words.txt', __FILE__)).
   split("\n").
   map(&:chomp)
-dic.shift
+3.times{dic.shift}
 
 SKEY = 'redis:asm:bench'
 
@@ -22,20 +24,25 @@ end
 r = Redis.current
 asm = Redis::Asm.new(r)
 
-needles = %w(a baz rifmino mskelngesol)
+needles = %w(a baz rifmino mskelngesol 元気です)
 
 Benchmark.bm(22) do |x|
   needles.each do |needle|
-    [1000, 10000, 50000, 100000].each do |s|
+    [1000, 10000, 100000].each do |s|
       setup_redis SKEY, dic, s
       x.report("%11s : %6d wd"%[needle, s]) { asm.search(SKEY, needle) }
     end
+    puts ""
   end
 end
 
-
-
-
-
+# output results
+puts "results from 100000\n"
+setup_redis SKEY, dic, 100000
+needles.each do |needle|
+  puts "#{needle} :"
+  p JSON.parse(asm.search(SKEY, needle))
+  puts ""
+end
 
 
