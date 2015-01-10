9460822f2a04c647ab92f406f46f707a5eb02291
Masato Yamaguchi <karateka2000@gmail.com>
2015-01-11 01:11:56 +0900

    modified for coveralls

--- redis-asm.gemspec
+++ redis-asm.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "coveralls"
+  spec.add_development_dependency "simplecov"
   spec.add_dependency 'redis', '~> 3.0'
   spec.add_dependency 'digest'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'redis'
 require 'redis/asm'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
